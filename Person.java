67aa5929afd9e48e46acd8e0a536482f26b27cd1
zarufa <zaripov.sf@gmal.com>
2019-06-09 00:43:10 +0300

    add Person class

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private final String name;
+    private final int age;
+    private final String sex;
+
+    public Person(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+}
